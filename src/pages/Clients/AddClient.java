d9e01f5e08541d774389e3175e8b3af8c9e41018
Deepak <ndvphanideepak@gmail.com>
2020-09-20 11:01:44 +0530

    Updated Add Client methods and element names

--- src/pages/Clients/AddClient.java
+++ src/pages/Clients/AddClient.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddClient {
 
@@ -51,87 +52,93 @@ public class AddClient {
 
 
 
-    /*@FindBy (id="client_state") WebElement element ;
+    @FindBy (id="client_state") WebElement txtClientName ;
 
-    @FindBy (id="client_zip") WebElement element ;
+    @FindBy (id="client_zip") WebElement txtZipCode ;
 
-    @FindBy (id="select2-client_country-container") WebElement element ;
+    @FindBy (id="select2-client_country-container") WebElement dropdownCountry ;
 
-    @FindBy (id="select2-client_gender-container") WebElement element ;
+    @FindBy (id="select2-client_gender-container") WebElement dropdownGender ;
 
-    @FindBy (id="client_birthdate") WebElement element ;
+    @FindBy (id="client_birthdate") WebElement calenderBirthdate ;
 
-    @FindBy (id="client_phone") WebElement element ;
+    @FindBy (id="client_phone") WebElement txtPhoneNumber ;
 
-    @FindBy (id="client_fax") WebElement element ;
+    @FindBy (id="client_fax") WebElement txtFaxNumber ;
 
-    @FindBy (id="client_mobile") WebElement element ;
+    @FindBy (id="client_mobile") WebElement txtMobileNumber ;
 
-    @FindBy (id="client_email") WebElement element ;
+    @FindBy (id="client_email") WebElement txtEmailAddress ;
 
-    @FindBy (id="client_web") WebElement element ;
+    @FindBy (id="client_web") WebElement txtWebAddress ;
 
-    @FindBy (id="client_vat_id") WebElement element ;
+    @FindBy (id="client_vat_id") WebElement txtVATID ;
 
-    @FindBy (id="client_tax_code") WebElement element ;*/
+    @FindBy (id="client_tax_code") WebElement txtTaxesCode ;
 
     public void setTxtState(String state)
     {
-
+    	txtClientName.sendKeys(state);
     }
 
     public void setTxtZip(String zip)
     {
-
+    	txtZipCode.sendKeys(zip);
     }
     public void setTxtCountry(String country)
     {
-
+    	Select s = new Select(dropdownCountry);
+    	s.selectByVisibleText("India");
+    	
     }
 
     public void setTxtGender(String gender)
     {
 
+    	Select s = new Select(dropdownGender);
+    	s.selectByIndex(0);
     }
 
     public void setTxtBirthdate(String birthdate)
     {
-
+    	
+    	
+    	
     }
 
     public void setTxtPhone(String phnone)
     {
-
+    	txtPhoneNumber.sendKeys(phnone);
     }
 
     public void setTxtFax(String fax)
     {
-
+    	txtFaxNumber.sendKeys(fax);
     }
 
     public void setTxtMobile(String mobile)
     {
-
+    	txtMobileNumber.sendKeys(mobile);
     }
 
     public void setTxtEmail(String email)
     {
-
+    	txtEmailAddress.sendKeys(email);
     }
 
     public void setTxtWeb(String web)
     {
-
+    	txtWebAddress.sendKeys(web);
     }
 
     public void setTxtVat(String vat)
     {
-
+    	txtVATID.sendKeys(vat);
     }
 
     public void setTxtTax(String tax)
     {
-
+    	txtTaxesCode.sendKeys(tax);
     }
 
 }
